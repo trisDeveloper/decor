7f7530d85b0ad913017d36a717fe83da61f73090
trisBouden <tris.bouden@mail.com>
2022-06-18 20:23:31 +0100

    another test

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
