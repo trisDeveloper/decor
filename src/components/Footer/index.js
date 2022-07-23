e66d28d29e8b8f6afc6e94f1ce38bd1783105742
trisBouden <tris.bouden@mail.com>
2022-07-23 20:47:49 +0100

    ..

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,13 +1,13 @@
-import React/* ,{ useState, useRef, useEffect }*/ from 'react';
+import React /* ,{ useState, useRef, useEffect }*/ from 'react';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import {
   faFacebookF,
   faInstagram,
   faLinkedinIn,
   faTwitter,
-} from "@fortawesome/free-brands-svg-icons";
+} from '@fortawesome/free-brands-svg-icons';
 import './index.scss';
 function Footer() {
   return (
@@ -28,12 +28,16 @@ function Footer() {
             </div>
             <div className="contact">
               <h2>Get In Touch</h2>
-              <form action="https://formsubmit.co/f640b5a7e168ab2b4cd4b5c7ac5b4269" method='POST'>
+              <form
+                action="https://formsubmit.co/f640b5a7e168ab2b4cd4b5c7ac5b4269"
+                method="POST"
+              >
                 <input
                   type="email"
                   name="email"
                   id="email"
-                  placeholder="Your Email" required
+                  placeholder="Your Email"
+                  required
                 />
                 <textarea
                   className="area"
@@ -42,39 +46,38 @@ function Footer() {
                   cols="20"
                   rows="2"
                   placeholder="Send a message..."
-                  value="Hi!"
-                >
-                </textarea>
+                  defaultValue="Hi!"
+                ></textarea>
                 <input className="submit" type="submit" value="Send" />
               </form>
             </div>
           </div>
         </div>
         <div className="footer-social">
-        <a href="https://instagram.com">
-          <FontAwesomeIcon
-            className="media ig"
-            icon={faInstagram}
-          ></FontAwesomeIcon>
-        </a>
-        <a href="https://facebook.com">
-          <FontAwesomeIcon
-            className="media fb"
-            icon={faFacebookF}
-          ></FontAwesomeIcon>
-        </a>
-        <a href="https://twitter.com">
-          <FontAwesomeIcon
-            className="media tw"
-            icon={faTwitter}
-          ></FontAwesomeIcon>
-        </a>
-        <a href="https://linkedin.com">
-          <FontAwesomeIcon
-            className="media in"
-            icon={faLinkedinIn}
-          ></FontAwesomeIcon>
-        </a>
+          <a href="https://instagram.com">
+            <FontAwesomeIcon
+              className="media ig"
+              icon={faInstagram}
+            ></FontAwesomeIcon>
+          </a>
+          <a href="https://facebook.com">
+            <FontAwesomeIcon
+              className="media fb"
+              icon={faFacebookF}
+            ></FontAwesomeIcon>
+          </a>
+          <a href="https://twitter.com">
+            <FontAwesomeIcon
+              className="media tw"
+              icon={faTwitter}
+            ></FontAwesomeIcon>
+          </a>
+          <a href="https://linkedin.com">
+            <FontAwesomeIcon
+              className="media in"
+              icon={faLinkedinIn}
+            ></FontAwesomeIcon>
+          </a>
         </div>
         <div className="rights">
           <p>
